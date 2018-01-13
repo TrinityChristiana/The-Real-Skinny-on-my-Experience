42958585e10bce99f06f423a08f961b5a4da7092
Trinity Terry <trinityterry@Trinitys-MacBook-Air.local>
2018-01-13 17:13:46 -0600

    Edit 1

---
+++ real-skinny-on-experience.js
@@ -0,0 +1,14 @@
+// Declare a variable (string) that stores your currentJob.
+var currentJob = "random";
+// Write an if/else if/else conditional statement that is based on each job on your resume and console logs a different statement based on how you felt about that job. 
+// For example: if (currentJob === 'junior instructor'), then console log: 'My current job is junior instructor. I LOVE helping! But they don't give me enough free food.' 
+if (currentJob === "student") {
+  console.log("My current job is being a student. I enjoy going because i get to see my friends and also learn a lot about music.")
+} else if (currentJob === "none") {
+  console.log("My current job is nonexistent. It gets boring doing nothing...")
+} else {
+  console.log("That's funny. You thought " + currentJob + " was my job. LOL. Nope. That's not me.")
+}
+// else if (currentJob === 'tooth fairy helper'), then console log 'My current job is tooth fairy helper. I am really just in it for the teeth.'
+// For the 'else' statement, write something funny about that not being one of your previous jobs.
+// Try changing your current job to another one of your past jobs. Watch how it console logs a different statement when you do.
